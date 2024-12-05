5ad8cceba68c4f00307f4bd9e18cb4c140216b40
rain44556 <bristy44556@gmail.com>
2024-12-06 02:28:46 +0600

    added movieCollection in mongo and create post api

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0czr5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -25,12 +25,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");    
+
+    const moviesCollection = client.db('moviesDB').collection('movies');
+
+    app.post('/movies', async(req, res)=>{
+      const newMovies = req.body;
+      const result = await moviesCollection.insertOne(newMovies);
+      // find().sort({ rating: -1})
+      console.log(result);
+    })
+
+  
+  
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
